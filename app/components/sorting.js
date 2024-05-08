db633db6bfe2b5743919cc9b2ebb11028093dda4
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 12:29:56 -1100

    create sorting component

---
+++ app/components/sorting.js
@@ -0,0 +1,14 @@
+// components/Sorting.js
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const SortingSidebar = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Sorting Sidebar Component</Typography>
+    </Box>
+  );
+};
+
+export default SortingSidebar;
