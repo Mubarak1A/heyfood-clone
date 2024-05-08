3ed7389bb5dd577be46182b9e099f51d7a6a83ac
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 12:27:31 -1100

    create foodlist component

---
+++ app/components/foodlists.js
@@ -0,0 +1,14 @@
+// components/FoodList.js
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const FoodList = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Food List Component</Typography>
+    </Box>
+  );
+};
+
+export default FoodList;
