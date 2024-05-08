7393f193b7f7c1ab9284d866d846a749f03f34be
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 12:28:34 -1100

    create bannerCarousel component

---
+++ app/components/bannerCarousel.js
@@ -0,0 +1,14 @@
+// components/BannerCarousel.js
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const BannerCarousel = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Banner Carousel Component</Typography>
+    </Box>
+  );
+};
+
+export default BannerCarousel;
