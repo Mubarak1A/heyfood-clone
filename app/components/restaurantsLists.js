9bbbcc7c4160f09fa7fd641aace4f254b4fc5e2d
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 12:59:47 -1100

    import all components to the main page

---
+++ app/components/restaurantsLists.js
@@ -0,0 +1,14 @@
+// components/RestaurantList.js
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const RestaurantList = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Restaurant List Component</Typography>
+    </Box>
+  );
+};
+
+export default RestaurantList;
