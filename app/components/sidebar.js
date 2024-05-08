1ce9bc0b560f49570d0bc360e36da43a17a085f0
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 12:25:27 -1100

    create sidebar component

---
+++ app/components/sidebar.js
@@ -0,0 +1,14 @@
+// components/Sidebar.js
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const Sidebar = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Sidebar Component</Typography>
+    </Box>
+  );
+};
+
+export default Sidebar;
