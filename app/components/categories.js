97b3a8459b239742f4c4966e002bcdf983733f05
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 12:26:49 -1100

    create categories component

---
+++ app/components/categories.js
@@ -0,0 +1,14 @@
+// components/Category.js
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const Category = () => {
+  return (
+    <Box>
+      <Typography variant="h6">Category Component</Typography>
+    </Box>
+  );
+};
+
+export default Category;
