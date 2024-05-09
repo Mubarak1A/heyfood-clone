2c8d6d34b19c95a25226725a40dd0eeadbb932c9
Mubarak <adesinamubarak123@gmail.com>
2024-05-08 14:09:49 -1100

    update sorting components content

--- app/components/sorting.js
+++ app/components/sorting.js
@@ -1,14 +1,28 @@
-// components/Sorting.js
+// components/SortingComponent.js
 import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import { Box, Typography, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { Sort as SortIcon } from '@mui/icons-material';
 
-const SortingSidebar = () => {
+const SortingComponent = ({ numberOfStores }) => {
   return (
-    <Box>
-      <Typography variant="h6">Sorting Sidebar Component</Typography>
+    <Box padding={2}>
+      <Typography variant="h6">All Stores</Typography>
+      <Typography variant="subtitle2" color="text.secondary">
+        {`${numberOfStores} Stores`}
+      </Typography>
+
+      <Typography variant="body1" sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+        <SortIcon /> Sort
+      </Typography>
+
+      <RadioGroup aria-label="sort" name="sort">
+        <FormControlLabel value="most-popular" control={<Radio />} label="Most Popular" />
+        <FormControlLabel value="highest-rated" control={<Radio />} label="Highest Rated" />
+        <FormControlLabel value="newest" control={<Radio />} label="Newest" />
+        <FormControlLabel value="most-rated" control={<Radio />} label="Most Rated" />
+      </RadioGroup>
     </Box>
   );
 };
 
-export default SortingSidebar;
+export default SortingComponent;
